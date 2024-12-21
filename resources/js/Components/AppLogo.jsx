8bd96352e50f74c984c341ea5f630fbaa353c4d2
clifforddonk <clifforddonk@gmail.com>
2024-12-21 21:25:17 +0000

    first commit

--- resources/js/Components/AppLogo.jsx
+++ resources/js/Components/AppLogo.jsx
@@ -5,7 +5,7 @@ const AppLogo = ({ className, variant, blue = false, props }) => {
         <Link className={variant} href="/" {...props} prefetch>
             <img
                 src={blue ? "" : ""}
-                alt="ChowApp Logo"
+                alt="Cho-App Logo"
                 className={`object-scale-down h-8 w-[8.8rem] xs:h-12 xs:w-[13.1rem]
                     transition-all duration-300 hover:scale-110 ${
                         className ? className : ""
