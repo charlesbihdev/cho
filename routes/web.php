97bd8914e96140c6a181f3cccd6116283c29e797
clifforddonk <clifforddonk@gmail.com>
2024-12-24 23:02:33 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/orders', function () {
+    return Inertia::render('Orders');});
+
+Route::get('/food', function () {
+        return Inertia::render('Food');});
+
+Route::get('/vendors', function () {
+            return Inertia::render('Vendors');});
+
+Route::get('/location', function () {
+                return Inertia::render('Location');});
+                
 require __DIR__.'/auth.php';
